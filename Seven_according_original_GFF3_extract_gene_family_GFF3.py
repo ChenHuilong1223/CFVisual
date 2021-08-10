1a86d9ce7ff07be853452d4b615c238e9a77d5af
ChenHuilong <77657266+ChenHuilong1223@users.noreply.github.com>
2021-08-10 19:12:44 +0800

    Update Seven_according_original_GFF3_extract_gene_family_GFF3.py

--- Seven_according_original_GFF3_extract_gene_family_GFF3.py
+++ Seven_according_original_GFF3_extract_gene_family_GFF3.py
@@ -1,5 +1,5 @@
 your_file = open('Sitalica_312_v2.2.gene.gff3') #your original GFF3 txt
-ID_file = open('Si.finally.treename.txt') #your gene id,Si1G000100.1
+ID_file = open('Si.PME.Ordered.ID.txtt') #your gene id,Si1G000100.1
 out_file = open('Si.PME.Structure.GFF3.txt', 'w') #out file
 
 for line in ID_file:
@@ -9,8 +9,6 @@ for line in ID_file:
 		if gene.replace('Si','Seita.') in row: #or： if gene in row:
 			result = row
 			out_file.write(result)
-#			print(fsy1[0].replace('scaffold_', 'Si') + "\t" + gene + "\t" + fsy1[3] + "\t" + fsy1[4])
 	your_file.seek(0,0)
 
-#make by chl
-#The script can be used to do more!		
+#make by Chen Huilong!	
