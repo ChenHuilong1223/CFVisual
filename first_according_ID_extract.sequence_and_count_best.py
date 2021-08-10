da19e991051dfae94f6e64e9ab5656457f720f81
ChenHuilong <77657266+ChenHuilong1223@users.noreply.github.com>
2021-08-10 18:25:21 +0800

    Update first_according_ID_extract.sequence_and_count_best.py

--- first_according_ID_extract.sequence_and_count_best.py
+++ first_according_ID_extract.sequence_and_count_best.py
@@ -23,22 +23,17 @@ chl_fasta[gene] = seq
 
 count = 0
 def y(x): # 这个y函数不调用，就是整行输出，把函数里的3个80改成60等其他数字，就是按照60等其他字符一行输出
-	# print('\n'.join([x[80*i:80*(i+1)] for i in range(math.ceil(len(x) / 80))]))
 	return '\n'.join([x[80*i:80*(i+1)] for i in range(math.ceil(len(x) / 80))])
 
 for line in gene_name:
 	my_gene = line.split()
 	my_id = my_gene[0]
 	if my_id in chl_fasta.keys():
-		# out_file.write('>' + my_id + '\n')
-		# for i in range(0,len(chl_fasta[my_id]),80):
-		# 	out_file.write(chl_fasta[my_id][i:i+80] + '\n')
-		# print(chl_fasta[my_id])
 		results = ">" + my_id + "\n" + y(chl_fasta[my_id]) + "\n"
 		out_file.write(results)
 		count += 1
 print(count)
-#make by chenhuilong
+#make by Chen Huilong!
 
 	
 		
